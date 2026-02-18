054e524d77b390aca1198b4c24c9d81c692d58dc
Elbert <2301101641@student.buksu.edu.ph>
2026-02-18 11:30:17 +0800

    translate spectrum v4

--- lib/widgets/audio_spectrum.dart
+++ lib/widgets/audio_spectrum.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:audio_waveforms/audio_waveforms.dart';
 
-class AudioSpectrum extends StatelessWidget {
+class AudioSpectrum extends StatefulWidget {
   final RecorderController recorderController;
 
   const AudioSpectrum({super.key, required this.recorderController});
 
   @override
+  State<AudioSpectrum> createState() => _AudioSpectrumState();
+}
+
+class _AudioSpectrumState extends State<AudioSpectrum> {
+  @override
   Widget build(BuildContext context) {
     return AudioWaveform(
       height: 50,
       width: MediaQuery.of(context).size.width * 0.8,
-      recorderController: recorderController,
-      waveStyle: const WaveStyle(
+      recorderController: widget.recorderController,
+      waveStyle: WaveStyle(
         waveColor: Colors.red,
         showDurationLabel: false,
         spacing: 8.0,

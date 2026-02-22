6e9b9629eeb1b885256c506641d56b6da548f478
Elbert <2301101641@student.buksu.edu.ph>
2026-02-22 15:47:14 +0800

    your progress

--- lib/screens/your_progress_screen.dart
+++ lib/screens/your_progress_screen.dart
@@ -1,9 +1,24 @@
-
+import 'package:fireb/screens/services/history_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import '../widgets/progress_card.dart';
 
-class YourProgressScreen extends StatelessWidget {
+class YourProgressScreen extends StatefulWidget {
   const YourProgressScreen({super.key});
+
+  @override
+  State<YourProgressScreen> createState() => _YourProgressScreenState();
+}
+
+class _YourProgressScreenState extends State<YourProgressScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Use a post-frame callback to ensure that the provider is available.
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<HistoryService>(context, listen: false).loadHistoryFromFirestore();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,32 +29,36 @@ class YourProgressScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: GridView.count(
-          crossAxisCount: 2,
-          crossAxisSpacing: 16,
-          mainAxisSpacing: 16,
-          children: const [
-            ProgressCard(
-              icon: Icons.book,
-              value: '0',
-              label: 'Words Learned',
-            ),
-            ProgressCard(
-              icon: Icons.local_fire_department,
-              value: '0',
-              label: 'Day Streak',
-            ),
-            ProgressCard(
-              icon: Icons.translate,
-              value: '0',
-              label: 'Translations',
-            ),
-            ProgressCard(
-              icon: Icons.timer,
-              value: '0h',
-              label: 'Time Spent',
-            ),
-          ],
+        child: Consumer<HistoryService>(
+          builder: (context, historyService, child) {
+            return GridView.count(
+              crossAxisCount: 2,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              children: [
+                ProgressCard(
+                  icon: Icons.book,
+                  value: historyService.history.length.toString(),
+                  label: 'Words Learned',
+                ),
+                const ProgressCard(
+                  icon: Icons.local_fire_department,
+                  value: '0',
+                  label: 'Day Streak',
+                ),
+                const ProgressCard(
+                  icon: Icons.translate,
+                  value: '0',
+                  label: 'Translations',
+                ),
+                const ProgressCard(
+                  icon: Icons.timer,
+                  value: '0h',
+                  label: 'Time Spent',
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
